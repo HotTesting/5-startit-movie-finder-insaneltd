71b5c5b4089f76480bbb2978e21563eee642ba78
iar <insaneltd@gmail.com>
2018-02-11 13:34:31 +0200

    Add Jasmine reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,5 +4,12 @@ module.exports.config = {
     specs: ['spec.ts'],
     directConnect: true,
     baseUrl: 'https://movies-finder.firebaseapp.com/',
-    SELENIUM_PROMISE_MANAGER: false
+    SELENIUM_PROMISE_MANAGER: false,
+    onPrepare: function() {
+        var ConsoleReporter = require('jasmine2-reporter').Jasmine2Reporter
+        var console_reporter_options = {
+            startingSpec: true
+        }
+        jasmine.getEnv().addReporter(new ConsoleReporter(console_reporter_options))
+    }
 }
